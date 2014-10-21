437c833fa5a4e94cc81762f8034ba9862acab1af
Scott Sanderson <scoutoss@gmail.com>
2014-10-21 13:56:45 -0400

    MAINT: Remove dependency on pandoc for setup.py.

--- setup.py
+++ setup.py
@@ -14,26 +14,11 @@ Copyright 2013 Gustav Arngarden
    limitations under the License.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-LONG_DESCRIPTION = None
-README_MARKDOWN = None
 
 with open('README.md') as markdown_source:
-    README_MARKDOWN = markdown_source.read()
-
-try:
-    import pandoc
-    pandoc.core.PANDOC_PATH = 'pandoc'
-    # Converts the README.md file to ReST, since PyPI uses ReST for formatting,
-    # This allows to have one canonical README file, being the README.md
-    doc = pandoc.Document()
-    doc.markdown = README_MARKDOWN
-    LONG_DESCRIPTION = doc.rst
-except ImportError:
-    # If pandoc isn't installed, e.g. when downloading from pip,
-    # just use the regular README.
-    LONG_DESCRIPTION = README_MARKDOWN
+    LONG_DESCRIPTION = markdown_source.read()
 
 setup(
     name='MongoDBProxy',
@@ -46,7 +31,6 @@ setup(
 #    license='',
 #    classifiers=[
 #    ],
-    setup_requires=['pyandoc'],
     install_requires=['pymongo'],
     url="https://github.com/arngarden/MongoDBProxy"
 )
